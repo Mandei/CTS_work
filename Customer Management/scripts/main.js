3b5f8613e49825fd8018146a1e1ed3c485dc43c1
Mandeep <mandeeprana1998@gmail.com>
2019-02-14 17:51:53 +0530

    Add files via upload

---
+++ Customer Management/scripts/main.js
@@ -0,0 +1,31 @@
+ 
+ 
+var curLat = null; //user location
+var curLon = null;
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+        window.alert("no location");
+    }
+}
+function showPosition(position) {
+    curLat = position.coords.latitude;
+    curLon = position.coords.longitude;
+}
+function initMap(){
+    
+  getLocation() //finds out user location to fomat the map
+  if (curLat == null){
+    curLat = 33.4484;   //if the user location cannot be found, set default ones
+    curLon = 112.0740;   // of boston
+    console.log("random locations");
+  }
+  var options = {
+    zoom:10,
+    center:{lat:curLat, lng:curLon}
+  }
+    var map = new google.maps.Map(document.getElementById("map"),options);
+ 
+}
